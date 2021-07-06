79baaf4f719a00660a5edaafc9b85e40b5d794e9
Original-Intention <851027263@qq.com>
2021-07-06 22:46:56 +0800

    每天记录算法题解题方法 20.有效的括号

--- src/main/java/com/example/algorithm/_20_Solution.java
+++ src/main/java/com/example/algorithm/_20_Solution.java
@@ -1,15 +1,21 @@
-package com.example.demo;
+package com.example.algorithm;
 
 import java.util.Stack;
 
-public class TestDemo {
+/**
+ * 20、有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+ * 有效字符串需满足：左括号必须用相同类型的右括号闭合。左括号必须以正确的顺序闭合。
+ */
+public class _20_Solution {
+
     public static void main(String[] args) {
         String s = "({[()]})";
         System.out.println(isValid(s))  ;
     }
-    //20、有效的括号
-    //给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
-    //有效字符串需满足：左括号必须用相同类型的右括号闭合。左括号必须以正确的顺序闭合。
+
+
+    //使用栈的数据结构，满足“先入后出，后入先出”
     public static boolean isValid(String s) {
         Stack<Character> stack = new Stack<Character>();
         for(char c : s.toCharArray()){
