fe929e9bccdfca34e1af355f7ba12eea01e5e71e
Original-Intention <851027263@qq.com>
2021-07-13 23:07:13 +0800

    5. 最长回文子串 6. Z 字形变换

---
+++ src/main/java/com/example/algorithm/_5_Solution.java
@@ -0,0 +1,40 @@
+package com.example.algorithm;
+
+/**
+ * 1. 两数之和
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出和为目标值 target的那两个整数，并返回它们的数组下标。
+ */
+public class _5_Solution {
+
+    //主方法
+    public static void main(String[] args) {
+        String s = "";
+        String result = longestPalindrome(s);
+        System.out.println(result);
+    }
+
+    public static String longestPalindrome(String s) {
+        if (s == null || s.length() < 1) {
+            return "";
+        }
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    public static int expandAroundCenter(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            --left;
+            ++right;
+        }
+        return right - left - 1;
+    }
+}
---
+++ src/main/java/com/example/algorithm/_6_Solution.java
@@ -0,0 +1,50 @@
+package com.example.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 6. Z 字形变换
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ * a   e   i   m
+ * b d f h j l n
+ * c   g   k
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ */
+public class _6_Solution {
+
+    //主方法
+    public static void main(String[] args) {
+        String s = "abcdefghijklmn";
+        int numRows = 3;
+        String result = convert(s,numRows);
+        System.out.println(result);
+    }
+
+    public static String convert(String s, int numRows) {
+        //若只有一行则返回s
+        if(numRows==1) return s;
+        StringBuffer result = new StringBuffer();
+        //建立集合代表行数
+        List<StringBuffer> list = new ArrayList<StringBuffer>();
+        for(int i=0;i<Math.min(numRows,s.length());i++){
+            list.add(new StringBuffer());
+        }
+        int curRow = 0;
+        boolean goingDown = false;
+        //遍历字符串，并走倒N字形，从上到下
+        for(char c : s.toCharArray()){
+            list.get(curRow).append(c);
+            if(curRow==0||curRow==numRows-1){
+                goingDown =!goingDown;
+            }
+            curRow+=goingDown?1:-1;
+        }
+        //最后拼接字符
+        for(StringBuffer str : list){
+            result.append(str);
+        }
+        return result.toString();
+    }
+}
